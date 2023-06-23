2242a2d849bcadd17858291b6acef2297037f46a
spoletaev <info.elekit@yandex.ru>
2023-06-23 16:09:49 +0300

    добавить ResponseEntity в контроллер

--- src/main/java/ru/job4j/bank/controller/AccountController.java
+++ src/main/java/ru/job4j/bank/controller/AccountController.java
@@ -2,6 +2,8 @@ package ru.job4j.bank.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.bank.dto.AccountDto;
@@ -17,7 +19,7 @@ public class AccountController {
     private final BankService bankService;
 
     @PostMapping("/")
-    public AccountDto addAccount(@RequestBody Map<String, String> body) {
+    public ResponseEntity<AccountDto> addAccount(@RequestBody Map<String, String> body) {
         String requisite = body.get("requisite");
         if (requisite == null) {
             throw new IllegalArgumentException("Parameter 'requisite' cannot be null");
@@ -35,14 +37,17 @@ public class AccountController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
                     String.format("Account with requisites [%s, %s] not saved", requisite, passport));
         }
-        return new AccountDto()
-                .setUserId(account.getId())
-                .setRequisite(account.getRequisite())
-                .setBalance(account.getBalance());
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new AccountDto()
+                        .setUserId(account.getId())
+                        .setRequisite(account.getRequisite())
+                        .setBalance(account.getBalance()));
     }
 
     @GetMapping("/")
-    public AccountDto findByRequisite(@RequestParam String passport, @RequestParam String requisite) {
+    public ResponseEntity<AccountDto> findByRequisite(@RequestParam String passport, @RequestParam String requisite) {
         if (passport == null) {
             throw new IllegalArgumentException("Parameter 'passport' cannot be null");
         }
@@ -53,9 +58,12 @@ public class AccountController {
                 () -> new ResponseStatusException(
                         HttpStatus.NOT_FOUND,
                         String.format("Account with requisites [%s, %s] not found", passport, requisite)));
-        return new AccountDto()
-                .setUserId(account.getId())
-                .setRequisite(account.getRequisite())
-                .setBalance(account.getBalance());
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new AccountDto()
+                        .setUserId(account.getId())
+                        .setRequisite(account.getRequisite())
+                        .setBalance(account.getBalance()));
     }
 }
